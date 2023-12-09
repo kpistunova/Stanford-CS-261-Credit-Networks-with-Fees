b69b38771b1b66d2f1a39ab8d9675f6e11fe35d1
Kate <kpistunova@stanford.edu>
2023-12-08 21:00:59 -0800

    generic plot

--- generic_plots.py
+++ generic_plots.py
@@ -31,6 +31,7 @@ for fee in df['fee'].unique():
         selected_indices = np.linspace(0, len(unique_capacities) - 1, 5, dtype=int)
         selected_capacities = unique_capacities[selected_indices]
         selected_fees = [fee, 0.0]
+        #x is node number
         df_filtered = df[(df['fee'].isin([fee, 0.0])) & (df['capacity'].isin(selected_capacities))]
         bg_color = plt.gcf().get_facecolor()
         palette = sns.color_palette('coolwarm', n_colors=len(df_filtered['capacity'].unique()))
@@ -41,18 +42,14 @@ for fee in df['fee'].unique():
         # Use 'fee' as the style variable
         sns.lineplot(data=df_filtered, x='nodes', y='avg_path_length', hue='capacity', style='fee', dashes=True, markers = True,
                      palette='coolwarm', hue_norm=matplotlib.colors.LogNorm(), ci='sd', alpha = 0.8)
-        plt.annotate(f'Fee: {fee}',
-                     xy=(0.5, 1), xycoords='axes fraction',
-                     xytext=(0, -10), textcoords='offset points',  # Offset by 10 points from the top
-                     horizontalalignment='center', verticalalignment='top',
-                     fontsize=14, bbox=dict(boxstyle="round,pad=0.3", edgecolor='gray', facecolor=bg_color))
+
         title = graph_type + ' graph'
         plt.title(title, fontsize=16)
         plt.xlabel('Node Number', fontsize=16)
         plt.ylabel('Average Path Length', fontsize=16)
         plt.xticks(fontsize=14)
         plt.yticks(fontsize=14)
-        filename = graph_type + '_graph' + f'_average_path_length_vs_node_number_fee_{f}.png'
+        filename = graph_type + '_graph' + f'_average_path_length_vs_node_number_fee_{fee}.png'
         file_path = os.path.join(new_directory_path, filename)
         sns.move_legend(ax, "upper left", bbox_to_anchor=(1, 1))
         plt.tight_layout()
@@ -65,11 +62,7 @@ for fee in df['fee'].unique():
         # Use 'fee' as the style variable
         sns.lineplot(data=df_filtered, x='nodes', y='success_rate', hue='capacity', style='fee', dashes=True, markers = True,
                      palette='coolwarm', hue_norm=matplotlib.colors.LogNorm(), ci='sd', alpha = 0.8)
-        plt.annotate(f'Fee: {fee}',
-                     xy=(0.5, 1), xycoords='axes fraction',
-                     xytext=(0, -10), textcoords='offset points',  # Offset by 10 points from the top
-                     horizontalalignment='center', verticalalignment='top',
-                     fontsize=14, bbox=dict(boxstyle="round,pad=0.3", edgecolor='gray', facecolor=bg_color))
+
         title = graph_type + ' graph'
         plt.title(title, fontsize=16)
         plt.xlabel('Node Number', fontsize=16)
@@ -77,10 +70,55 @@ for fee in df['fee'].unique():
         plt.xticks(fontsize=14)
         plt.yticks(fontsize=14)
         sns.move_legend(ax, "upper left", bbox_to_anchor=(1, 1))
-        filename = graph_type + '_graph' + f'_success_rate_vs_node_number_fee_{f}.png'
+        filename = graph_type + '_graph' + f'_success_rate_vs_node_number_fee_{fee}.png'
         file_path = os.path.join(new_directory_path, filename)
         plt.tight_layout()
         fig.savefig((file_path), dpi=300)
         plt.show()
         plt.close(fig)
 
+palette = sns.color_palette("coolwarm", n_colors=df['nodes'].nunique())
+
+for fee in df['fee'].unique():
+    if fee != 0.0:
+        # x is capacity
+        df_filtered = df[(df['fee'].isin([fee, 0.0]))]
+        fig, ax = plt.subplots(figsize=(8 , 6 / 1.2), dpi=300)
+
+        # Use 'fee' as the style variable
+        sns.lineplot(data=df_filtered, x='capacity', y='avg_path_length', hue='nodes', style='fee', dashes=True, markers = True,
+                     palette=palette, hue_norm=matplotlib.colors.LogNorm(), ci='sd', alpha = 0.8)
+        title = graph_type + ' graph'
+        plt.title(title, fontsize=16)
+        plt.xlabel('Capacity', fontsize=16)
+        plt.ylabel('Average Path Length', fontsize=16)
+        plt.xticks(fontsize=14)
+        plt.yticks(fontsize=14)
+        filename = graph_type + '_graph' + f'_average_path_length_vs_capacity_fee_{fee}.png'
+        file_path = os.path.join(new_directory_path, filename)
+        sns.move_legend(ax, "upper left", bbox_to_anchor=(1, 1))
+        plt.tight_layout()
+        fig.savefig((file_path), dpi=300)
+        plt.show()
+        plt.close(fig)
+
+        fig, ax = plt.subplots(figsize=(8, 6 / 1.2), dpi=300)
+
+        # Use 'fee' as the style variable
+        sns.lineplot(data=df_filtered, x='capacity', y='success_rate', hue='nodes', style='fee', dashes=True, markers = True,
+                     palette=palette, hue_norm=matplotlib.colors.LogNorm(), ci='sd', alpha = 0.8)
+        title = graph_type + ' graph'
+        plt.title(title, fontsize=16)
+        plt.xlabel('Capacity', fontsize=16)
+        plt.ylabel('Success Rate', fontsize=16)
+        plt.xticks(fontsize=14)
+        plt.yticks(fontsize=14)
+        sns.move_legend(ax, "upper left", bbox_to_anchor=(1, 1))
+        filename = graph_type + '_graph' + f'_success_rate_vs_capacity_fee_{fee}.png'
+        file_path = os.path.join(new_directory_path, filename)
+        plt.tight_layout()
+        fig.savefig((file_path), dpi=300)
+        plt.show()
+        plt.close(fig)
+
+print('terue')
