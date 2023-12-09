9695afceae5732fa7be49a344d1fc4d151ce4870
Kate <kpistunova@stanford.edu>
2023-12-08 21:14:02 -0800

    generic plot

--- generic_plots.py
+++ generic_plots.py
@@ -22,6 +22,7 @@ name = 'random_er_graph_node_vs_fee_all_capacity_after_fix_correct.pkl'
 graph_type = 'Random'
 df = pd.read_pickle(name)
 unique_capacities = np.sort(df['capacity'].unique())
+unique_nodes = np.sort(df['nodes'].unique())
 
 #------fixed---fees--dif---capacity
 
@@ -43,7 +44,7 @@ for fee in df['fee'].unique():
         sns.lineplot(data=df_filtered, x='nodes', y='avg_path_length', hue='capacity', style='fee', dashes=True, markers = True,
                      palette='coolwarm', hue_norm=matplotlib.colors.LogNorm(), ci='sd', alpha = 0.8)
 
-        title = graph_type + ' graph'
+        title = graph_type + f' graph, fee = {fee}'
         plt.title(title, fontsize=16)
         plt.xlabel('Node Number', fontsize=16)
         plt.ylabel('Average Path Length', fontsize=16)
@@ -63,7 +64,7 @@ for fee in df['fee'].unique():
         sns.lineplot(data=df_filtered, x='nodes', y='success_rate', hue='capacity', style='fee', dashes=True, markers = True,
                      palette='coolwarm', hue_norm=matplotlib.colors.LogNorm(), ci='sd', alpha = 0.8)
 
-        title = graph_type + ' graph'
+        title = graph_type + f' graph, fee = {fee}'
         plt.title(title, fontsize=16)
         plt.xlabel('Node Number', fontsize=16)
         plt.ylabel('Success Rate', fontsize=16)
@@ -77,18 +78,20 @@ for fee in df['fee'].unique():
         plt.show()
         plt.close(fig)
 
-palette = sns.color_palette("coolwarm", n_colors=df['nodes'].nunique())
-
 for fee in df['fee'].unique():
     if fee != 0.0:
+        selected_indices = np.linspace(0, len(unique_nodes) - 1, 5, dtype=int)
+        selected_nodes = unique_nodes[selected_indices]
+        selected_fees = [fee, 0.0]
+        #x is node number
+        df_filtered = df[(df['fee'].isin([fee, 0.0])) & (df['nodes'].isin(selected_nodes))]
         # x is capacity
-        df_filtered = df[(df['fee'].isin([fee, 0.0]))]
         fig, ax = plt.subplots(figsize=(8 , 6 / 1.2), dpi=300)
 
         # Use 'fee' as the style variable
         sns.lineplot(data=df_filtered, x='capacity', y='avg_path_length', hue='nodes', style='fee', dashes=True, markers = True,
-                     palette=palette, hue_norm=matplotlib.colors.LogNorm(), ci='sd', alpha = 0.8)
-        title = graph_type + ' graph'
+                     palette='coolwarm', hue_norm=matplotlib.colors.LogNorm(), ci='sd', alpha = 0.8, legend = 'full')
+        title = graph_type + f' graph, fee = {fee}'
         plt.title(title, fontsize=16)
         plt.xlabel('Capacity', fontsize=16)
         plt.ylabel('Average Path Length', fontsize=16)
@@ -106,8 +109,8 @@ for fee in df['fee'].unique():
 
         # Use 'fee' as the style variable
         sns.lineplot(data=df_filtered, x='capacity', y='success_rate', hue='nodes', style='fee', dashes=True, markers = True,
-                     palette=palette, hue_norm=matplotlib.colors.LogNorm(), ci='sd', alpha = 0.8)
-        title = graph_type + ' graph'
+                     palette='coolwarm', hue_norm=matplotlib.colors.LogNorm(), ci='sd', alpha = 0.8, legend = 'full')
+        title = graph_type + f' graph, fee = {fee}'
         plt.title(title, fontsize=16)
         plt.xlabel('Capacity', fontsize=16)
         plt.ylabel('Success Rate', fontsize=16)
